07e12e172d678ea876043e37f790c753065f34f3
michal <michal.now03@gmail.com>
2024-12-30 17:26:32 +0100

    Randomizer and functions

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <unordered_map>
 #include <random>
+#include <algorithm>
 
 using namespace std;
 
@@ -12,20 +13,22 @@ struct Vertex {
 
 struct Input {
     string dna;
-    int length;
-    int k;
-    int delta;
-    int positive_error;
-    int negative_error;
-    bool probable_positive_error;
-    bool rep_allowed;
+    int length = 400;
+    int k = 8;
+    int delta = 2;
+    int positive_error = 0;
+    int negative_error = 0;
+    bool probable_positive_error = false;
+    bool rep_allowed = true;
 };
+
+Input input;
 
 //Mapa odpowiadająca za nasz graf
 unordered_map<string, Vertex> graph;
 Vertex Head;
 
-string generate_dna(int length) {
+string generateDNA(int length) {
     string dna;
     char tab_nucleotides[4] = {'A', 'C', 'G', 'T'};
     for(int i = 0; i < length; i++) {
@@ -34,13 +37,23 @@ string generate_dna(int length) {
     return dna;
 }
 
-unordered_map<string, Vertex> generate_spectrum(const Input &input) {
-    unordered_map<string, Vertex> spectrum;
+void randomize_spectrum(unordered_map<string, Vertex> &spectrum) {
+    std::vector<std::pair<std::string, Vertex>> vec(spectrum.begin(), spectrum.end());
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(vec.begin(), vec.end(), g);
+    spectrum.clear();
+    for (const auto &pair : vec) {
+        spectrum.insert(pair);
+    }
+}
+
+unordered_map<string, Vertex> generate_flawless_spectrum() {
+    unordered_map<string, Vertex> flawless_spectrum;
     unordered_map<string, Vertex> ERROR;
     ERROR.insert(make_pair("ERROR", Vertex()));
     int offset;
-    string new_oligo = "";
-    //GENEROWANE SPEKTRUM BEZBŁĘDNE
+    string new_oligo;
     bool negative_error_detected = false;
     for(int i = 0; i < input.length - input.k + 1; i++) {
 
@@ -55,28 +68,35 @@ unordered_map<string, Vertex> generate_spectrum(const Input &input) {
         Vertex new_vertex;
         new_vertex.oligo_nucleotide = new_oligo;
         if(!input.negative_error && !input.rep_allowed) {
-            if(spectrum.contains(new_vertex.oligo_nucleotide)) {;
+            if(flawless_spectrum.contains(new_vertex.oligo_nucleotide)) {;
                 return ERROR;
             }
         }
-        spectrum.insert(make_pair(new_oligo, new_vertex));
+       flawless_spectrum.insert(make_pair(new_oligo, new_vertex));
         new_oligo = "";
     }
-    //BŁĘDY NEGATYWNE
+    return flawless_spectrum;
+}
+
+void generate_negative_error(unordered_map<string, Vertex> &spectrum) {
     for(int i = 0; i < input.negative_error; i++) {
         int random_index = rand() % int(spectrum.size());
         auto it = spectrum.begin();
         advance(it, random_index);
         spectrum.erase(it);
     }
-    //BŁĘDY POZYTYWNE
+}
+
+void generate_positive_error(unordered_map<string, Vertex> &spectrum) {
+    int offset;
+    string new_oligo = "";
     for(int i = 0; i < input.positive_error; i++) {
         if(!input.probable_positive_error) {
             offset = rand() % (2*input.delta + 1) - input.delta;
             Vertex new_vertex;
-            new_oligo = generate_dna(input.k + offset);
+            new_oligo = generateDNA(input.k + offset);
             while(spectrum.contains(new_oligo)) {
-                new_oligo = generate_dna(input.k + offset);
+                new_oligo = generateDNA(input.k + offset);
             }
             new_vertex.oligo_nucleotide = new_oligo;
             spectrum.insert(make_pair(new_vertex.oligo_nucleotide, new_vertex));
@@ -89,9 +109,9 @@ unordered_map<string, Vertex> generate_spectrum(const Input &input) {
             int place = int(it->first.size() - 1) /2;
             // GENERUJEMY I SPRAWDZAMY CZY ZNAK SIE ZMIENIŁ
             char current_sign = it->first.at(place);
-            char change = generate_dna(1)[0];
+            char change = generateDNA(1)[0];
             while(change == current_sign) {
-                change = generate_dna(1)[0];
+                change = generateDNA(1)[0];
             }
             // TWORZYMY NOWĄ PODSEKWENCJE
             new_oligo = it->second.oligo_nucleotide;
@@ -104,9 +124,9 @@ unordered_map<string, Vertex> generate_spectrum(const Input &input) {
             place = int(it->first.size() - 1);
             // GENERUJEMY I SPRAWDZAMY CZY ZNAK SIE ZMIENIŁ
             current_sign = it->first.at(place);
-            change = generate_dna(1)[0];
+            change = generateDNA(1)[0];
             while(change == current_sign) {
-                change = generate_dna(1)[0];
+                change = generateDNA(1)[0];
             }
             // TWORZYMY NOWĄ PODSEKWENCJE
             new_oligo = it->second.oligo_nucleotide;
@@ -117,15 +137,26 @@ unordered_map<string, Vertex> generate_spectrum(const Input &input) {
             spectrum.insert(make_pair(new_oligo, new_vertex2));
             // USUWAMY WYBRAŃCA
             spectrum.erase(it->first);
-            // INKEREMNTUJEMY I BO DODALIŚMY DWA BŁĘDY
+            // INKEREMNTUJEMY i BO DODALIŚMY DWA BŁĘDY
             i++;
         }
 
     }
+}
+
+unordered_map<string, Vertex> generateSpectrum() {
+    unordered_map<string, Vertex> spectrum;
+    do {
+        input.dna = generateDNA(input.length);
+        spectrum = generate_flawless_spectrum();
+    } while (spectrum.contains("ERROR"));
+    generate_negative_error(spectrum);
+    generate_positive_error(spectrum);
+    randomize_spectrum(spectrum);
     return spectrum;
 }
 
-void print_spectrum(){
+void printSpectrum(){
     int i = 1;
     for(auto &it : graph) {
         cout<< i <<": " << it.first << endl;
@@ -133,24 +164,11 @@ void print_spectrum(){
     }
 }
 
+
+
 int main() {
 srand(time(nullptr));
-    Input input;
-    input.length = 700;
-    input.k = 9;
-    input.delta = 2;
-    input.positive_error = 0;
-    input.negative_error = 0;
-    input.probable_positive_error = true;
-    input.rep_allowed = false;
-    int TEST = 0;
-    do {
-        input.dna = generate_dna(input.length);
-        TEST++;
-        cout<<"TEST: "<<TEST<<endl;
-        graph = generate_spectrum(input);
-    } while(graph.contains("ERROR") || TEST > 10000);
+    graph = generateSpectrum();
     cout<<input.dna<<endl;
-    print_spectrum();
-    cout<<"TEST: "<<TEST<<endl;
+    printSpectrum();
 }
